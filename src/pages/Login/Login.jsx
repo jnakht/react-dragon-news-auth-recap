0d6f3742bdcd2b412cbcd8fd5e2f5718bf45d4f1
jnakht <jisan.cse.cu@gmail.com>
2025-03-30 17:50:26 +0600

    login route added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3>THis is login form</h3>
+        </div>
+    );
+};
+
+export default Login;
