26acc3a03100c17ba0afc32f9b98d2d35da2b51a
jnakht <jisan.cse.cu@gmail.com>
2025-04-02 10:22:31 +0600

    login changed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,43 @@
 import Navbar from "../Shared/Navbar/Navbar";
 
 const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const userEmail = form.get('email');
+        const userPassword = form.get('password');
+        console.log(userEmail, userPassword)
+    }
+
     return (
         <div>
             <Navbar></Navbar>
-            <h3>THis is login form</h3>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col ">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form onSubmit={handleLogin}>
+                            <div className="card-body">
+                                <fieldset className="fieldset">
+                                    <label className="fieldset-label">Email</label>
+                                    <input type="email" 
+                                    name="email"
+                                    className="input" placeholder="Email" />
+                                    <label className="fieldset-label">Password</label>
+                                    <input type="password" className="input" 
+                                    name="password"
+                                    placeholder="Password" />
+                                    <div><a className="link link-hover">Forgot password?</a></div>
+                                    <button className="btn btn-neutral mt-4">Login</button>
+                                </fieldset>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
