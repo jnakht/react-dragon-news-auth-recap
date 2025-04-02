9a53adbea2bc941b18671f4917ea34cba2421fbc
jnakht <jisan.cse.cu@gmail.com>
2025-04-02 17:09:17 +0600

    after login navigate to state or homepage using useLoaction and location

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
     const { logIn } = useContext(AuthContext); 
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log('this is the location in login page: ', location)
     const handleLogin = (e) => {
         e.preventDefault();
         const form = new FormData(e.currentTarget);
@@ -15,6 +18,9 @@ const Login = () => {
         logIn(userEmail, userPassword)
         .then(result => {
             console.log(result.user);
+
+            // navigate to state or homepage
+            navigate(location?.state ? location.state : '/');
         })
         .catch(error => {
             console.error(error);
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
+    console.log('this is the location in private route', location)
     if (loading) {
         return <div className="h-full w-full  flex justify-center items-center"><span className="loading loading-infinity loading-xl h-full mt-[300px]"></span></div>
     }
@@ -11,7 +13,7 @@ const PrivateRoute = ({children}) => {
         return children;
     }
 
-    return <Navigate to='/login'></Navigate>
+    return <Navigate state={location.pathname} to='/login'></Navigate>
 };
 
 export default PrivateRoute;
