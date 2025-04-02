1aa58ad39d34851e961561ae0bcff6fed4426563
jnakht <jisan.cse.cu@gmail.com>
2025-04-02 23:36:27 +0600

    updated firebase file for security

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// console.log('in the firebase file: ', import.meta.env.AUTH_APIKEY)
 const firebaseConfig = {
-  apiKey: "AIzaSyBhMKlbGbF3IuChmR5Eo3qDUORxXzxUQg4",
-  authDomain: "react-dragon-news-auth-2704c.firebaseapp.com",
-  projectId: "react-dragon-news-auth-2704c",
-  storageBucket: "react-dragon-news-auth-2704c.firebasestorage.app",
-  messagingSenderId: "1053993186505",
-  appId: "1:1053993186505:web:6330993e1f3a327c528918"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
